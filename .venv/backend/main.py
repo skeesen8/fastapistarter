e7e67de6735329e76a884ab4f84a3a690b2feb90
SKLAPTOP\skees <skeesen8@gmail.com>
2024-12-30 14:44:26 -0600

    added pathing

--- .venv/backend/main.py
+++ .venv/backend/main.py
@@ -6,6 +6,8 @@ from enum import Enum
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
@@ -16,9 +18,9 @@ async def root():
     return {"message": "this finally works"}
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+# @app.get("/items/{item_id}")
+# async def read_item(item_id):
+#     return {"item_id": item_id}
 
 @app.get("/users")
 async def read_users():
@@ -31,3 +33,13 @@ async def get_model(model_name:ModelName):
     if model_name.value == "lenet":
         return {"model_name":model_name, "message": "LeCNN all the Images"}
     return{"model_name":model_name,"message":"Have SOme ReSiduALS"}
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
