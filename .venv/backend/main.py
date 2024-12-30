19c1fd3217273c9df0a1836fc35594920c0d63f3
SKLAPTOP\skees <skeesen8@gmail.com>
2024-12-30 14:02:01 -0600

    added class and models

--- .venv/backend/main.py
+++ .venv/backend/main.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI
 
+from enum import Enum
+
+
+
 app = FastAPI()
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "this finally works"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+@app.get("/models/{model_name}")
+async def get_model(model_name:ModelName):
+    if model_name is ModelName.alexnet:
+        return{"model_name":model_name,"message":"Deep Learngin FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name":model_name, "message": "LeCNN all the Images"}
+    return{"model_name":model_name,"message":"Have SOme ReSiduALS"}
